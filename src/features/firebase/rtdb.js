95e4965808da642eefa45fadd8d8241b2757e396
Miguel <miguel@MacBook-Pro-3.local>
2026-04-17 15:07:17 -0500

    Remove compiled artifacts, update .gitignore

--- src/features/firebase/rtdb.js
+++
@@ -1,9 +0,0 @@
-import { ref as dbRef, set, get, update, onValue, remove, onDisconnect } from 'firebase/database';
-import { getFirebaseRTDB } from './setup';
-export function getRoomRef(code) {
-    const db = getFirebaseRTDB();
-    if (!db)
-        return null;
-    return dbRef(db, `rooms/${code}`);
-}
-export { set, get, update, onValue, remove, onDisconnect };
